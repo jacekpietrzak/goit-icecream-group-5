3ec2ffb7fce98b3cedc41a447cc425db7b8d65ca
jacekpietrzak <jacekpietrzak@me.com>
2022-07-21 19:46:46 +0000

    Deploying to gh-pages from @ jacekpietrzak/goit-icecream-group-5@23a215eb097610682e7b135327bec66d94793d6e 🚀

--- src.d699419c.js
+++ src.d699419c.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-icecream-group-5/src.35ec33bf.js.map
+//# sourceMappingURL=/goit-icecream-group-5/src.d699419c.js.map
